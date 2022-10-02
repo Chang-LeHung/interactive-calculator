44a013bbb12da98b73848cabca794d835aec5fe9
Chang-LeHung <799207539@qq.com>
2022-10-02 12:21:28 +0800

    update signal handler

--- src/main/java/handler/QuitHandler.java
+++ src/main/java/handler/QuitHandler.java
@@ -1,2 +1,37 @@
-package handler;public class QuitHandler {
+package handler;
+
+import sun.misc.Signal;
+import sun.misc.SignalHandler;
+
+import java.io.IOException;
+
+public class QuitHandler implements SignalHandler {
+  @Override
+  public void handle(Signal sig) {
+    byte[] info = new byte[1024];
+    try {
+      while (true) {
+        System.out.println("Do you want to quit? [y/n]");
+        int off = System.in.read(info);
+        String s = new String(info);
+        if (s.equalsIgnoreCase("y")) {
+          System.exit(-1);
+        }
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    Signal.handle(new Signal("TERM"), new QuitHandler());
+    Signal.handle(new Signal("INT"), new QuitHandler());
+    byte[] b = new byte[1024];
+    System.in.read(b);
+    System.out.println("hello world 1");
+    System.in.read(b);
+    System.out.println("hello world 2");
+    System.in.read(b);
+    System.out.println("hello world 3");
+  }
 }
